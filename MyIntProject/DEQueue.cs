0e89aee435af5550b1eb578c0c72cceb328c81ab
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-05-22 20:15:28 +0400

    init queue

---
+++ MyIntProject/DEQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIntProject
+{
+    class DEQueue<T>
+    {
+        private List<T> items;
+
+        public DEQueue()
+        {
+            items = new List<T>();
+        }
+
+        public void pushBack(T item)
+        {
+            items.Add(item);
+        }
+
+        public void pushFront(T item)
+        {
+            items.Insert(0, item);
+        }
+
+        public T popFront()
+        {
+            T item = items.FirstOrDefault<T>();
+            if(items.Count > 0)
+            {
+                items.RemoveAt(0);
+            }
+            return item;
+        }
+
+        public T popBack()
+        {
+            T item = items.LastOrDefault<T>();
+            if(items.Count > 0)
+            {
+                items.RemoveAt(items.Count - 1);
+            }           
+            return item;
+        }
+
+        public T back()
+        {
+            return items.LastOrDefault<T>();
+        }
+
+        public T front()
+        {
+            return items.FirstOrDefault<T>();
+        }
+
+        public int size()
+        {
+            return items.Count;
+        }
+
+        public void clear()
+        {
+            items.Clear();
+        }
+
+        public T[] toArray()
+        {
+            return items.ToArray();
+        }
+    }
+}
---
+++ MyIntProject/DEQueueUnitTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MyIntProject
+{
+    [TestFixture]
+    class DEQueueUnitTests
+    {
+        DEQueue<string> q;
+
+        [SetUp] 
+        public void init()
+        {
+            q = new DEQueue<string>();
+        }
+
+        [Test]
+        public void checkEmptyQueue()
+        {
+            Assert.IsNull(q.back());
+            Assert.IsNull(q.front());
+            Assert.IsNull(q.popBack());
+            Assert.IsNull(q.popFront());
+        }
+
+        [Test]
+        public void checkPush()
+        {
+            q.pushFront("1");
+            Assert.AreEqual(q.front(), "1");
+            Assert.AreEqual(q.back(), "1");
+
+            q.pushFront("2");
+            Assert.AreEqual(q.front(), "2");
+            Assert.AreEqual(q.back(), "1");
+
+            q.pushBack("3");
+            Assert.AreEqual(q.front(), "2");
+            Assert.AreEqual(q.back(), "3");
+        }
+
+        [Test]
+        public void checkPop()
+        {
+            q.pushBack("1");
+            q.pushBack("2");
+            q.pushBack("3");
+
+            Assert.AreEqual(q.popFront(), "1");
+            Assert.AreEqual(q.popBack(), "3");
+            Assert.AreEqual(q.popFront(), "2");
+        }
+
+        [Test]
+        public void checkSize()
+        {
+            Assert.AreEqual(q.size(), 0);
+
+            q.pushBack("1");
+            q.pushBack("2");
+            q.pushBack("3");
+
+            Assert.AreEqual(q.size(), 3);
+            q.pushFront("4");
+            Assert.AreEqual(q.size(), 4);
+            q.popBack();
+            Assert.AreEqual(q.size(), 3);
+            q.popFront();
+            Assert.AreEqual(q.size(), 2);
+        }
+
+        [Test]
+        public void checkClear()
+        {
+            q.pushBack("1");
+            q.pushFront("2");
+
+            Assert.AreEqual(q.front(), "2");
+            Assert.AreEqual(q.back(), "1");
+            Assert.AreEqual(q.size(), 2);
+
+            q.clear();
+
+            Assert.IsNull(q.front());
+            Assert.IsNull(q.back());
+            Assert.AreEqual(q.size(), 0);
+        }
+
+        [Test]
+        public void checkToArray()
+        {
+            q.pushBack("1");
+            q.pushBack("2");
+
+            Assert.AreEqual(q.toArray(), new string[] { "1", "2" });
+
+            q.popBack();
+
+            q.pushFront("3");
+
+            q.pushBack("4");
+
+            Assert.AreEqual(q.toArray(), new string[] { "3", "1", "4" });
+
+            q.clear();
+
+            Assert.AreEqual(q.toArray(), new string[] { });
+        }
+    }
+}
