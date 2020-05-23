65a77af4065cd600b15ffd7f217e06b8993cfbec
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-05-23 11:12:59 +0400

    added max, min, abs, toString, longValue, compareTo methods

---
+++ UnitTests/MyInt.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIntProject
+{
+    public class MyInt
+    {
+        private int[] value;
+
+        public MyInt(long number)
+        {
+            bool isPositive = number >= 0;
+            List<int> items = new List<int>();
+
+            number = Math.Abs(number);
+
+            while(number / 10 != 0)
+            {
+                int rest = (int)(number % 10);
+                items.Insert(0, rest);
+                number /= 10;
+            }
+
+            items.Insert(0, (int) (number % 10));
+            items.Insert(0, isPositive ? 0 : 1);
+
+            value = validate(items.ToArray());
+        }
+
+        public MyInt(string number)
+        {
+            bool isPositive = number[0] != '-';
+            List<int> items = number.Where(num => num != '-').Select(num => int.Parse(num.ToString())).ToList();
+            items.Insert(0, isPositive ? 0 : 1);
+
+            value = validate(items.ToArray());
+        }
+
+        public MyInt(int[] number)
+        {
+            value = validate(number);            
+        }
+
+        private int[] validate(int[] number)
+        {
+            int[] result = number.Take(1).Concat(number.Skip(1).FirstOrDefault(num => num != 0) != 0 
+                    ? number.Skip(1).SkipWhile(num => num == 0) 
+                    : number.Skip(1).Take(1)
+                ).ToArray();
+
+            if (result[0] == 1 && result.Length == 2 && result[1] == 0)
+            {
+                result[0] = 0;
+            }
+            return result;
+        }
+
+
+        public int[] getValue()
+        {
+            return value;
+        }
+
+        public MyInt add(MyInt b)
+        {
+            int[] valueB = b.getValue();
+            int[] result = new int[Math.Max(value.Length, valueB.Length) + 1];
+            int temp = 0;
+            for(int i = 0; i < result.Length - 1; i++)
+            {
+                int firstNum = (value.Length - 2 < i ? 0 : value[value.Length - 1 - i]);
+                int secondNum = (valueB.Length - 2 < i ? 0 : valueB[valueB.Length - 1 - i]);
+                int sum = firstNum + secondNum + temp;
+                result[result.Length - 1 - i] = sum % 10;
+                temp = sum / 10;
+            }
+            return new MyInt(result);
+        }
+
+        public MyInt substract(MyInt b)
+        {
+            int[] valueB = b.getValue();
+            valueB[0] = valueB[0] == 0 ? 1 : 0;
+            return add(new MyInt(valueB));
+        }
+
+        /*public MyInt multiply(MyInt b)
+        {
+
+        }
+
+        public MyInt divide(MyInt b)
+        {
+
+        }*/
+
+        public MyInt max(MyInt b)
+        {
+            int[] valueB = b.getValue();
+
+            bool isPositiveA = value[0] == 0;
+            bool isPositiveB = valueB[0] == 0;
+            if(isPositiveA != isPositiveB)
+            {
+                return isPositiveA ? this : b;
+            }
+
+            int lengthA = value.Length;
+            int lengthB = valueB.Length;
+            if (lengthA != lengthB)
+            {
+                return lengthA > lengthB ? this : b;
+            }
+
+            int numA = 0, numB = 0, i = 1;
+            do
+            {
+                numA = value[i];
+                numB = valueB[i];
+                i++;
+            } while (numA == numB);
+
+            return numA > numB ? this : b;
+        }
+
+        public MyInt min(MyInt b)
+        {
+            return max(b) == this ? b : this;
+        }
+
+        public MyInt abs()
+        {
+            int[] newValue = value;
+            value[0] = 0;
+            return new MyInt(newValue);
+        }
+
+        public bool compareTo(MyInt b)
+        {
+            return value.SequenceEqual(b.getValue());
+        }
+
+        public MyInt gcd(MyInt b)
+        {
+            MyInt a = abs();
+            b = b.abs();
+            while(!a.compareTo(b))
+            {
+                if (a.max(b) == a)
+                {
+                    a = a.substract(b);
+                }
+                else
+                {
+                    b = b.substract(a);
+                }
+            }
+
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return String.Concat((value[0] == 0 ? "" : "-"), string.Join(string.Empty, value.Skip(1).ToArray()));
+        }
+
+        public long longValue()
+        {
+            return long.Parse(ToString());
+        }
+    }
+}
---
+++ UnitTests/MyIntUnitTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MyIntProject
+{
+    [TestFixture]
+    class MyIntUnitTests
+    {
+        [Test]
+        public void testCreate()
+        {
+            Assert.AreEqual((new MyInt(123)).ToString(), "123");
+            Assert.AreEqual((new MyInt(123)).longValue(), 123);
+
+            Assert.AreEqual((new MyInt(-123)).ToString(), "-123");
+            Assert.AreEqual((new MyInt(-123)).longValue(), -123);
+
+            Assert.AreEqual((new MyInt("123")).ToString(), "123");
+            Assert.AreEqual((new MyInt("123")).longValue(), 123);
+
+            Assert.AreEqual((new MyInt("-123")).ToString(), "-123");
+            Assert.AreEqual((new MyInt("-123")).longValue(), -123);
+
+            Assert.AreEqual((new MyInt(new int[] { 0, 1, 2, 3 })).ToString(), "123");
+            Assert.AreEqual((new MyInt(new int[] { 0, 1, 2, 3 })).longValue(), 123);
+
+            Assert.AreEqual((new MyInt(new int[] { 1, 1, 2, 3 })).ToString(), "-123");
+            Assert.AreEqual((new MyInt(new int[] { 1, 1, 2, 3 })).longValue(), -123);
+
+            Assert.AreEqual((new MyInt(0)).ToString(), "0");
+            Assert.AreEqual((new MyInt(0)).longValue(), 0);
+
+            Assert.AreEqual((new MyInt(-0)).ToString(), "0");
+            Assert.AreEqual((new MyInt(-0)).longValue(), 0);
+
+            Assert.AreEqual((new MyInt("0")).ToString(), "0");
+            Assert.AreEqual((new MyInt("0")).longValue(), 0);
+
+            Assert.AreEqual((new MyInt("-0")).ToString(), "0");
+            Assert.AreEqual((new MyInt("-0")).longValue(), 0);
+
+            Assert.AreEqual((new MyInt(new int[] { 0, 0 })).ToString(), "0");
+            Assert.AreEqual((new MyInt(new int[] { 0, 0 })).longValue(), 0);
+
+            Assert.AreEqual((new MyInt(new int[] { 1, 0 })).ToString(), "0");
+            Assert.AreEqual((new MyInt(new int[] { 1, 0 })).longValue(), 0);
+        }
+
+        [Test]
+        public void testCompareTo()
+        {
+            Assert.IsTrue((new MyInt(new int[] { 1, 1, 2 })).compareTo(new MyInt(-12)));
+            Assert.IsTrue((new MyInt("-0")).compareTo(new MyInt(new int[] { 0, 0, 0, 0 })));
+            Assert.IsTrue((new MyInt(4213)).compareTo(new MyInt("04213")));
+            Assert.IsFalse((new MyInt(130)).compareTo(new MyInt(new int[] { 1, 1, 3, 0 })));
+        }
+
+        [Test]
+        public void testAbs()
+        {
+            Assert.AreEqual((new MyInt(new int[] { 1, 1, 3 })).abs().longValue(), 13);
+            Assert.AreEqual((new MyInt("-0250")).abs().longValue(), 250);
+            Assert.AreEqual((new MyInt(520)).abs().longValue(), 520);
+            Assert.AreEqual((new MyInt("-12")).abs().longValue(), 12);
+        }
+
+        [Test]
+        public void testMaxMin()
+        {
+            MyInt a = new MyInt(200);
+            MyInt b = new MyInt(199);
+            MyInt c = new MyInt("201");
+            MyInt d = new MyInt(-200);
+
+            Assert.AreEqual(b.max(a), a.max(b));
+            Assert.AreEqual(a.max(b), a);
+            Assert.AreEqual(c.max(a), c);
+            Assert.AreEqual(c.max(b), c);
+            Assert.AreEqual(b.max(d), b);
+
+            Assert.AreEqual(b.min(d), d.min(b));
+            Assert.AreEqual(a.min(b), b);
+            Assert.AreEqual(a.min(c), a);
+            Assert.AreEqual(d.min(a), d);
+            Assert.AreEqual(c.min(d), d);            
+        }
+    }
+}
